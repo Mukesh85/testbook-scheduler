dadbdd7663314d8b0b3acb3e5e292687bb1f7e67
Mukesh P <mukesh85tek@gmail.com>
2017-12-21 08:17:37 +0530

    Updating app.js to redirect on default url.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.set('port', 3000);
 
 app.use(express.static(path.join(__dirname,"www")));
 
+app.get("/",function(req,res){
+	return res.redirect("/index.html");
+})
+
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
