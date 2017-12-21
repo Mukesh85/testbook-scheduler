bfa972e4fb427232892341ae6b929a6e79b8af3a
Mukesh P <mukesh85tek@gmail.com>
2017-12-21 08:19:00 +0530

    updating app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.set('port', 3000);
+const PORT = process.env.PORT || 5000
+
+app.set('port', PORT);
 
 app.use(express.static(path.join(__dirname,"www")));
 
